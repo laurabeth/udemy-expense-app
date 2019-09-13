bf2ab32a79f004219cd3b3288e896cae38a2c74d
Laura Bhayani <lbhayani@sentryone.com>
2019-09-13 15:02:10 -0400

    finish vid 89, array destructuring. skipped exercises.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,1 +1,53 @@
-console.log("redux-101");
+import { createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  const incrementBy =
+    typeof action.incrementBy === "number" ? action.incrementBy : 1;
+  const decrementBy =
+    typeof action.decrementBy === "number" ? action.decrementBy : 1;
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + incrementBy,
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - decrementBy,
+      };
+    case "RESET":
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+let x = 0;
+while (x < 3) {
+  store.dispatch({
+    incrementBy: 5,
+    type: "INCREMENT",
+  });
+
+  x++;
+}
+
+store.dispatch({
+  type: "RESET",
+});
+
+store.dispatch({
+  type: "DECREMENT",
+});
+
+store.dispatch({
+  decrementBy: 10,
+  type: "DECREMENT",
+});
+
+unsubscribe();
